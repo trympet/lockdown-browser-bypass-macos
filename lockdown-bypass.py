23e06a1ed58ba828f9f6023dd6ff8406494bd337
TrymPet <trym@flogard.no>
2020-07-28 23:23:25 +0200

    add license

--- lockdown-bypass.py
+++ lockdown-bypass.py
@@ -1,6 +1,13 @@
-# (c) 2018 by Trym Lund Flogard
-# https://github.com/trympet/lockdown-browser-bypass-macos
-# This code is licensed under MIT license (see LICENSE for details) 
+#---------------------------------------------------------  
+#
+# Copyright (c) Trym Lund Flogard. All rights reserved.  
+# This code is licensed under the MIT License (MIT).  
+# THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF  
+# ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY  
+# IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR  
+# PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.  
+#  
+#---------------------------------------------------------
 from Cocoa import *
 import objc
 import time
