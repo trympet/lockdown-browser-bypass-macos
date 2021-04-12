07c14d68de58223d4f607f278dd3c4bf469c06d7
rohanvsuri <rsuri@irusmail.com>
2021-04-11 20:20:09 -0400

    added opening multiple instances

--- lockdown-bypass.py
+++ lockdown-bypass.py
@@ -1,12 +1,12 @@
-#---------------------------------------------------------  
+#---------------------------------------------------------
 #
-# Copyright (c) Trym Lund Flogard. All rights reserved.  
-# This code is licensed under the MIT License (MIT).  
-# THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF  
-# ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY  
-# IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR  
-# PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.  
-#  
+# Copyright (c) Trym Lund Flogard. All rights reserved.
+# This code is licensed under the MIT License (MIT).
+# THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
+# ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
+# IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
+# PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
+#
 #---------------------------------------------------------
 from Cocoa import *
 import objc
@@ -15,19 +15,24 @@ import psutil
 import sys
 import os
 
-waitTime = 10 # time before activating
+waitTime = 15 # time before activating
 appName = "Helium" #application to bring to front
 
 def isHeliumRunning():
     #Iterate over the all the running process
+    pids = []
     for proc in psutil.process_iter():
+        print(proc)
         try:
             # Check if process name contains the given name string.
             if appName.lower() in proc.name().lower():
-                return proc.pid
+                pids.append(proc.pid) #append to pids list if more than one instance running
         except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
             pass
-    return 0;
+    if pids:
+        return pids
+    else:
+        return 0;
 
 pid = isHeliumRunning()
 if (pid == 0):
@@ -36,14 +41,17 @@ if (pid == 0):
   inp = input()[0].lower()
   if(inp == 'y'):
     # opens helium
+    inp2 = input("How many instances?")
     print("opening " + appName)
-    os.system("open -a " + appName)
+    for i in range(int(inp2)):
+        os.system(f"open -n {appName}.app")
     time.sleep(0.5)
     pid = isHeliumRunning() # assigns new pid
   else:
     print("okay closing...")
     time.sleep(1)
     exit()
+
 print(appName + " found!")
 print("PID:" + str(pid))
 
@@ -51,12 +59,13 @@ print("you have " + str(waitTime) + "seconds")
 for i in reversed(range(waitTime)):
   time.sleep(1)
   print(str(i) + "...")
-  
-# hides and unhides app
-x = NSRunningApplication.runningApplicationWithProcessIdentifier_(pid)
-x.hide()     
-time.sleep(1)
 
-x.unhide()
-time.sleep(2)    
+# hides and unhides app
+for i in pid:
+    x = NSRunningApplication.runningApplicationWithProcessIdentifier_(i)
+    x.hide()
+    time.sleep(1)
+
+    x.unhide()
+    time.sleep(2)
 print("DONE")
